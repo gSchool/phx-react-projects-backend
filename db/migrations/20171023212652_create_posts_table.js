0dfcd08d71b2e63f33b42238f19630a05e809228
John <jaboyak@gmail.com>
2017-10-27 08:18:08 -0700

    fixed post img_url default link

--- db/migrations/20171023212652_create_posts_table.js
+++ db/migrations/20171023212652_create_posts_table.js
@@ -6,7 +6,7 @@ exports.up = function (knex, Promise) {
     table.text('content');
     table.integer('votes').default(0);
     table.timestamp('created_at').default(knex.fn.now());
-    table.text('image_url').default("http://store-nz.monsroyale.com/c.3934103/app-nz/img/no_image_available.jpeg?resizeid=3&resizeh=1000&resizew=1000").notNullable()
+    table.text('image_url').default("http://via.placeholder.com/250x250").notNullable()
   });
 };
 
