c2334667de8220e50392c16a9d160041631db4f0
John <jaboyak@gmail.com>
2017-10-27 15:11:59 -0700

    fixed cameras seed

--- db/seeds/10_cameras.js
+++ db/seeds/10_cameras.js
@@ -9,21 +9,21 @@ exports.seed = function(knex, Promise) {
         image: 'http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg',
         rating: 4,
         price: 369.99,
-        onSale: true
+        on_sale: true
       }, {
         id: 2,
         title: 'Canon EOS 70D',
         image: 'http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg',
         rating: 2,
         price: 1099.0,
-        onSale: false
+        on_sale: false
       }, {
         id: 3,
         title: 'Nikon D810A',
         image: 'http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg',
         rating: 3,
         price: 3796.95,
-        onSale: true
+        on_sale: true
       }]);
     });
 };
