8948ec941f68155d73fe17215af4c59accc8ccb4
John <jaboyak@gmail.com>
2017-12-01 11:00:26 -0700

    updated car seeds

--- db/seeds/04_cars.js
+++ db/seeds/04_cars.js
@@ -1,4 +1,3 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('cars').del()
@@ -11,6 +10,7 @@ exports.seed = function(knex, Promise) {
           model: 'Civic',
           miles: 135000,
           price: 3500,
+          photo_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/1992-1995_Honda_Civic_sedan_--_03-21-2012.JPG/1200px-1992-1995_Honda_Civic_sedan_--_03-21-2012.JPG'
         },
         {
           year: 2004,
@@ -18,6 +18,7 @@ exports.seed = function(knex, Promise) {
           model: 'PT Cruiser',
           miles: 155000,
           price: 3500,
+          photo_url: 'http://www.chryslercolors.info/wp-content/uploads/2011/06/2004-Chrysler-PT-Cruiser-Touring-Edition-2.jpg'
         },
         {
           year: 2006,
@@ -25,6 +26,7 @@ exports.seed = function(knex, Promise) {
           model: '6',
           miles: 85000,
           price: 4700,
+          photo_url: 'http://1-photos.motorcar.com/used-2006-mazda-mazda6-s-13131-17016736-4-640.jpg'
         },
         {
           year: 2017,
@@ -32,6 +34,7 @@ exports.seed = function(knex, Promise) {
           model: 'Volt',
           miles: 15000,
           price: 33220,
+          photo_url: 'https://images.hgmsites.net/med/2016-chevrolet-volt-catskill-mountains-ny-dec-2015_100539555_m.jpg'
         },
         {
           year: 2009,
@@ -39,6 +42,7 @@ exports.seed = function(knex, Promise) {
           model: 'Camry LE',
           miles: 139000,
           price: 6700,
+          photo_url: 'https://upload.wikimedia.org/wikipedia/commons/2/2d/2009_Toyota_Camry_LE.jpg'
         },
         {
           year: 2006,
@@ -46,6 +50,7 @@ exports.seed = function(knex, Promise) {
           model: 'Sedona',
           miles: 124000,
           price: 2500,
+          photo_url: 'https://i.ytimg.com/vi/Hy1d4DbpECg/maxresdefault.jpg'
         },
         {
           year: 2005,
@@ -53,6 +58,7 @@ exports.seed = function(knex, Promise) {
           model: 'Prius',
           miles: 184000,
           price: 5500,
+          photo_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/2nd_Toyota_Prius.jpg/1200px-2nd_Toyota_Prius.jpg'
         },
         {
           year: 2003,
@@ -60,6 +66,7 @@ exports.seed = function(knex, Promise) {
           model: '350Z',
           miles: 140000,
           price: 6000,
+          photo_url: 'https://static.cargurus.com/images/site/2008/05/15/16/09/2003_nissan_350z_track-pic-61257-1600x1200.jpeg'
         },
       ]);
     });
--- routes/carsRoutes.js
+++ routes/carsRoutes.js
@@ -19,20 +19,45 @@ router.get('/:id', function(req, res) {
 });
 
 router.post('/', function (req, res) {
-  knex('cars').insert(req.body).then(() => {
-    knex('cars').select().then(cars => res.json(cars))
+  let newCar = {
+    year: req.body.year,
+    make: req.body.make,
+    model: req.body.model,
+    miles: req.body.miles,
+    price: req.body.price,
+    photo_url: req.body.photo_url
+  }
+  knex('cars').returning('id').insert(newCar).then((result) => {
+    let car_id = result[0]
+    knex('cars_locations')
+      .insert({
+        car_id: car_id,
+        location_id: req.body.location_id
+      })
+      .then(() => {
+        knex('cars_locations')
+          .select('cars.id', 'cars.year', 'cars.make', 'cars.model', 'cars.miles', 'cars.price', 'cars.photo_url', 'cars_locations.location_id')
+          .join('cars', 'cars_locations.car_id', 'cars.id')
+          .then(inventory => res.json(inventory))
+    });
   });
 });
 
 router.patch('/:id', function (req, res) {
   knex('cars').update(req.body).where('id', req.params.id).then(function () {
-    knex('cars').select().then(cars => res.json(cars))
+    knex('cars_locations')
+      .select('cars.id', 'cars.year', 'cars.make', 'cars.model', 'cars.miles', 'cars.price', 'cars.photo_url', 'cars_locations.location_id')
+      .join('cars', 'cars_locations.car_id', 'cars.id')
+      .then(inventory => res.json(inventory))
   });
 });
 
 router.delete('/:id', function (req, res) {
   knex('cars').del().where('id', req.params.id).then(function () {
-    knex('cars').select().then(cars => res.json(cars))
+    knex('cars_locations')
+      .select('cars.id', 'cars.year', 'cars.make', 'cars.model', 'cars.miles', 'cars.price', 'cars.photo_url', 'cars_locations.location_id')
+      .join('cars', 'cars_locations.car_id', 'cars.id')
+      .then(inventory => res.json(inventory))
   });
 });
 
