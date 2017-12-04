71e7ae1438517b144ef51d48a9d8e9eb1086bbb4
JohnA <jaboyak@gmail.com>
2017-12-04 11:07:27 -0700

    removed ids from cameras seed file

--- db/seeds/10_cameras.js
+++ db/seeds/10_cameras.js
@@ -4,21 +4,18 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('cameras').insert([{
-        id: 1,
         title: 'Nikon D3100 DSLR',
         image: 'http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg',
         rating: 4,
         price: 369.99,
         on_sale: true
       }, {
-        id: 2,
         title: 'Canon EOS 70D',
         image: 'http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg',
         rating: 2,
         price: 1099.0,
         on_sale: false
       }, {
-        id: 3,
         title: 'Nikon D810A',
         image: 'http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg',
         rating: 3,
