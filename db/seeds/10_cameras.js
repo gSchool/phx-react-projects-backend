6bebe2efc0318a39261f25c0126a9ba57342fdb5
JohnA <jaboyak@gmail.com>
2017-12-05 13:21:49 -0700

    Update 10_cameras.js

--- db/seeds/10_cameras.js
+++ db/seeds/10_cameras.js
@@ -8,19 +8,22 @@ exports.seed = function(knex, Promise) {
         image: 'http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg',
         rating: 4,
         price: 369.99,
-        on_sale: true
+        on_sale: true,
+        in_cart: false
       }, {
         title: 'Canon EOS 70D',
         image: 'http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg',
         rating: 2,
         price: 1099.0,
-        on_sale: false
+        on_sale: false,
+        in_cart: false
       }, {
         title: 'Nikon D810A',
         image: 'http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg',
         rating: 3,
         price: 3796.95,
-        on_sale: true
+        on_sale: true,
+        in_cart: false
       }]);
     });
 };
