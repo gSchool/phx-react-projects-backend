601792e455eef5aae8d545ae2842880fac7a3a3b
John <jaboyak@gmail.com>
2018-02-09 04:24:19 -0700

    added login route to people

--- routes/peopleRoutes.js
+++ routes/peopleRoutes.js
@@ -2,9 +2,21 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
   knex('people').select().then(people => res.json(people))
+});
+
+router.post('/login', function (req, res, next) {
+  knex('people')
+    .where('email', req.body.email)
+    .andWhere('password', req.body.password)
+    .then(person => {
+      if(person.length < 1) {
+        res.sendStatus(400)
+      } else {
+        res.json(person[0])
+      }
+    })
 });
 
 router.get('/:id', function (req, res) {
